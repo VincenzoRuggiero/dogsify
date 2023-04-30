41dd371ec3891be811c6029aac98896d471d1cac
VincenzoRuggiero <vincenzoru@live.it>
2023-04-30 18:48:24 +0200

    Selection makes matching API call

--- main.js
+++ main.js
@@ -7,3 +7,25 @@ async function start() {
 }
 
 start();
+
+function createBreedList(breedList) {
+  document.getElementById("breed").innerHTML = `
+    <select onchange="loadByBreed(this.value)">
+        <option>Choose a dog breed</option>
+        ${Object.keys(breedList)
+          .map((breed) => {
+            return `<option>${breed}</option>`;
+          })
+          .join("")}
+    
+    </select>
+    `;
+}
+
+async function loadByBreed(breed) {
+  if (breed != "Choose a dog breed") {
+    const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`);
+    const data = await response.json();
+    console.log(data);
+  }
+}
