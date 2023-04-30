75b2a87e47fc8fdee609d2a520e384eed24b22be
VincenzoRuggiero <vincenzoru@live.it>
2023-04-30 19:41:50 +0200

    Create functions to handle slideshow

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let timer;
+let deleteFirstPhotoDelay;
+
 async function start() {
   const response = await fetch("https://dog.ceo/api/breeds/list/all");
   const data = await response.json();
@@ -30,9 +33,41 @@ async function loadByBreed(breed) {
 }
 
 function createSlideShow(images) {
+  let currentPosition = 0;
+  clearInterval(timer);
+  clearTimeout(deleteFirstPhotoDelay);
   document.getElementById("slideshow").innerHTML = ` <div
     class="slide"
     style="
       background-image: url('${images[0]}');
+    "></div>
+    <div
+    class="slide"
+    style="
+      background-image: url('${images[1]}');
     "></div>`;
+
+  currentPosition += 2;
+  timer = setInterval(nextSlide, 3000);
+
+  function nextSlide() {
+    document.getElementById("slideshow").insertAdjacentHTML(
+      "beforeend",
+      `</div>
+    <div
+    class="slide"
+    style="
+      background-image: url('${images[currentPosition]}');
+    "></div>`
+    );
+    deleteFirstPhotoDelay = setTimeout(
+      () => document.querySelector(".slide").remove,
+      1000
+    );
+    if (currentPosition + 1 >= images.length) {
+      currentPosition = 0;
+    } else {
+      currentPosition++;
+    }
+  }
 }
